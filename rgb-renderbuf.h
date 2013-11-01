33e41d56097b43efd4abce96c85e8003dbca4ecc
Justin McGirr <mcgirr.ju@gmail.com>
2013-11-01 12:09:04 -0700

    Add C (RGB) version of render.

---
+++ rgb-renderbuf.h
@@ -0,0 +1,6 @@
+static const struct {
+  unsigned int 	 width;
+  unsigned int 	 height;
+  unsigned int 	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
+  unsigned char	 pixel_data[144 * 144 * 3 + 1];
+} gimp_image;
